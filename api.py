08d71a292324a96e60172a2831292b49710130eb
samuel Kabbaj <kabbaj.samuel@gmail.com>
2018-03-22 15:09:12 +0100

    [fix] - change api to set and get speed

--- api.py
+++ api.py
@@ -1,24 +1,21 @@
 import json
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 file_to_save = "direction.json"
 
 
-def update_json(value, axe, status="normal"):
+def update_json(right, left, status="normal"):
     with open(file_to_save) as json_file:
         data = json.load(json_file)
-        if axe == "y":
-            data["status"] = status
-            data["direction"]["y"] = value
-        if axe == "x":
-            data["status"] = status
-            data["direction"]["x"] = value
         if status == "autonomous":
             data["status"] = status
-            data["direction"]["x"] = ""
-            data["direction"]["y"] = ""
+            data["vitesse"]["right"] = ""
+            data["vitesse"]["left"] = ""
+        data["status"] = status
+        data["vitesse"]["right"] = right
+        data["vitesse"]["left"] = left
         return data
 
 
@@ -27,42 +24,23 @@ def health():
     return "Ok"
 
 
-@app.route("/forward")
-def set_direction_to_forward():
-    data = update_json("forward", "y", "normal")
-    with open(file_to_save, 'w') as json_file:
-        json.dump(data, json_file)
-        json_file.close()
-        return "Ok", 200
+@app.route("/action", methods=["POST"])
+def set_direction():
+
+    if request.method == "POST":
+        right = request.args.get("right")
+        left = request.args.get("left")
+        data = update_json(right, left, "normal")
+        with open(file_to_save, 'w') as json_file:
+            json.dump(data, json_file)
+            json_file.close()
+            return "Ok", 200
+
+    else:
+        return "It's a POST Route", 405
 
 
-@app.route("/reverse")
-def set_direction_to_reverse():
-    data = update_json("reverse", "y", "normal")
-    with open(file_to_save, 'w') as json_file:
-        json.dump(data, json_file)
-        json_file.close()
-        return "Ok", 200
-
-
-@app.route("/left")
-def set_direction_to_left():
-    data = update_json("left", "x", "normal")
-    with open(file_to_save, 'w') as json_file:
-        json.dump(data, json_file)
-        json_file.close()
-        return "Ok", 200
-
-
-@app.route("/right")
-def set_direction_to_right():
-    data = update_json("right", "x", "normal")
-    with open(file_to_save, 'w') as json_file:
-        json.dump(data, json_file)
-        return "Ok", 200
-
-
-@app.route("/autonomous")
+@app.route("/autonomous", methods=["POST"])
 def set_autonomous_mode():
     data = update_json("", "", "autonomous")
     with open(file_to_save, 'w') as json_file:
@@ -71,15 +49,15 @@ def set_autonomous_mode():
         return "Ok", 200
 
 
-@app.route("/stop")
+@app.route("/stop", methods=["POST"])
 def shutdown():
     with open(file_to_save, 'w') as json_file:
-        json.dump({"direction": {"x": "", "y": ""}, "status": "normal"}, json_file)
+        json.dump({"vitesse": {"right": "", "left": ""}, "status": "normal"}, json_file)
         json_file.close()
         return "Ok", 200
 
 
-@app.route("/direction")
+@app.route("/direction", methods=["GET"])
 def get_direction():
     with open(file_to_save) as json_file:
         data = json.load(json_file)
