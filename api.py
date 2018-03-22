d808149995da2dbecbab3099c18b91531d7d310b
samuel Kabbaj <kabbaj.samuel@gmail.com>
2018-03-22 19:12:19 +0100

    [feat] - algo to convert axe to speed and docker-compose

--- api.py
+++ api.py
@@ -4,6 +4,8 @@ from flask import Flask, request
 app = Flask(__name__)
 
 file_to_save = "direction.json"
+right = 0
+left = 0
 
 
 def update_json(right, left, status="normal"):
@@ -19,6 +21,25 @@ def update_json(right, left, status="normal"):
         return data
 
 
+def convert_x_and_y_to_speed(x, y):
+    global right
+    global left
+    if y > 0:
+        if x > 0:  # On tourne a droite en marche avant
+            left = y
+            right = 100 - x
+        else:  # On tourne a gauche en marche avant
+            right = 100
+            left = 100 + x
+    else:
+        if x < 0:  # On tourne a droite en marche arriere
+            right = -100 + x
+            left = -100
+        else:  # On tourne a gauche en marche arriere
+            left = -100 - x
+            right = -100
+
+
 @app.route("/health")
 def health():
     return "Ok"
@@ -28,8 +49,9 @@ def health():
 def set_direction():
 
     if request.method == "POST":
-        right = request.args.get("right")
-        left = request.args.get("left")
+        x = request.args.get("x")
+        y = request.args.get("y")
+        convert_x_and_y_to_speed(x, y)
         data = update_json(right, left, "normal")
         with open(file_to_save, 'w') as json_file:
             json.dump(data, json_file)
